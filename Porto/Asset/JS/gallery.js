0b5f5981c1a155a4128a647e912b4030d68da043
alaltair41-a11y <alaltair41@gmail.com>
2026-03-16 22:15:46 +0700

    Add files via upload

---
+++ Porto/Asset/JS/gallery.js
@@ -0,0 +1,99 @@
+const track = document.querySelector(".slider-track");
+const slides = document.querySelectorAll(".slide");
+const containergallery = document.querySelector(".slider-container");
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let index = 0;
+let slideWidth = slides[0].offsetWidth;
+
+function updateSlide(){
+track.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+next.onclick = () =>{
+if(index < slides.length - 3){
+index++;
+updateSlide();
+}
+}
+
+prev.onclick = () =>{
+if(index > 0){
+index--;
+updateSlide();
+}
+}
+
+window.addEventListener("resize",()=>{
+slideWidth = slides[0].offsetWidth;
+updateSlide();
+});
+
+
+// DRAG SYSTEM
+
+let isDragging = false;
+let startX;
+let scrollStart;
+
+containergallery.addEventListener("mousedown",(e)=>{
+isDragging = true;
+startX = e.pageX;
+scrollStart = index;
+containergallery.style.cursor = "grabbing";
+});
+
+containergallery.addEventListener("mouseup",()=>{
+isDragging = false;
+containergallery.style.cursor = "grab";
+});
+
+containergallery.addEventListener("mouseleave",()=>{
+isDragging = false;
+});
+
+containergallery.addEventListener("mousemove",(e)=>{
+
+if(!isDragging) return;
+
+let move = e.pageX - startX;
+
+if(move > 100){
+index = Math.max(index-1,0);
+updateSlide();
+isDragging=false;
+}
+
+if(move < -100){
+index = Math.min(index+1,slides.length-3);
+updateSlide();
+isDragging=false;
+}
+
+});
+
+
+// TOUCH SWIPE (Mobile)
+
+containergallery.addEventListener("touchstart",(e)=>{
+startX = e.touches[0].clientX;
+});
+
+containergallery.addEventListener("touchend",(e)=>{
+
+let endX = e.changedTouches[0].clientX;
+let diff = startX - endX;
+
+if(diff > 50){
+index = Math.min(index+1,slides.length-3);
+}
+
+if(diff < -50){
+index = Math.max(index-1,0);
+}
+
+updateSlide();
+
+});
